f26e2deb91af4ffcaa1f1db6e451acbbc71bd26b
Junaid Islam <Junaid.2005p@gmail.com>
2024-03-04 10:45:28 +0000

    fixed the Transactions class paramterised constructor

--- Transactions.java
+++ Transactions.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 public class Transactions
 {
     public String TransactionID;
-    public publicKey sender;
-    public publicKey receiver;
+    public PublicKey sender;
+    public PublicKey receiver;
     public float value;
     public byte[] signature;
 
     public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
     public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
 
-    public Transaction(PublicKey from, PublicKey to, float value,  ArrayList<TransactionInput> inputs) {
+    public Transactions(PublicKey from, PublicKey to, float value,  ArrayList<TransactionInput> inputs) {
         this.sender = from;
         this.receiver = to;
         this.value = value;
